afff458d211c6bfeea38a3fdb71bd0c06908fa14
Alberto I. Cruz Salamán <alberto.cruz6@upr.edu>
2021-10-23 17:14:39 -0400

    Added first tweet interaction

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ client = discord.Client()
 tw_handler = get_api_handler()
 if tw_handler is None:
 	print("Invalid tw bot account found!")
+else:
+	 tw_handler.update_status("AllSeeBot ONLINE!");
 
 # Client events all exist on the Discord API 
 @client.event
