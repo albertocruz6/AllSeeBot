d50cc30a4d00886409445df2354317d9f6265ec2
Alberto I. Cruz Salamán <alberto.cruz6@upr.edu>
2021-10-24 09:25:03 -0400

    Added exception statment Fixed typo in stack

--- utils/discord/search_bot_client.py
+++ utils/discord/search_bot_client.py
@@ -149,7 +149,7 @@ class SearchBot(discord.Client):
 					try:
 						user_r = self.tw_handler.get_user(user_id=user)
 						tweets = self.tw_handler.user_timeline(screen_name=user_r.screen_name,count = 1)
-						self.user_tracktwitter_dictionary[user] = tweets[0].id
+						self.user_track_dictionary[user] = tweets[0].id
 						writeLastTweet(user, self.user_track_dictionary[user])
 						if self.user_track_channel:
 							await self.user_track_channel.send("https://twitter.com/twitter/statuses/{0}".format(self.user_track_dictionary[user]))
